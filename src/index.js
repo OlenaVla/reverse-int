51beedb38da3c020d8ca907626fd20a4bbc56937
OlenaVla <evlasenko409@gmail.com>
2022-01-11 13:35:38 +0200

    feat: implement reverse func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let strNum = Math.abs(n).toString();
+  let strRes = '';
+  for (var i = strNum.length-1; i>=0; i--)
+  {
+     strRes += strNum.charAt(i);
+  }
+  return Number(strRes);
 }
